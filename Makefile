33e2e732556eb8dd9a3fbcee504138baf3495546
nNown <wojciechmar2000@gmail.com>
2020-11-09 12:31:39 +0100

    Changed system to Debian

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 CXX = g++
 CPPFLAGS = -Wall -Wextra -Wno-unused-parameter -Iinclude/ -g -Og -std=c++17
+LFLAGS = -lstdc++fs
 
 all: main
 
 main: src/main.cpp src/graph.cpp src/graph_parser.cpp
-	$(CXX) $(CPPFLAGS) -o $@ $^
+	$(CXX) $(CPPFLAGS) -o $@ $^ $(LFLAGS)
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 
 int main(int argc, char* argv[]) {
     GraphParser* parser = GraphParser::GraphParserInstance();
-    // Graph* test = parser->LoadGraph(std::string(argv[1]));
-    // for(auto& vertex : test->Vertices()) {
-    //     for(auto& edge : vertex.Edges()) {
-    //         std::cout << edge << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+    Graph* test = parser->LoadGraph(std::string(argv[1]));
+    for(auto& vertex : test->Vertices()) {
+        for(auto& edge : vertex.Edges()) {
+            std::cout << edge << " ";
+        }
+        std::cout << std::endl;
+    }
 
     // Graph example(3);
     // example.AddEdge(0, 1);
